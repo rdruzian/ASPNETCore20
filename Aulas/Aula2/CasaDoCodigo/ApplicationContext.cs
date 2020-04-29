f6ba7543a1944650674b91053bc5d9b929aadabb
rdruzian <rendruzian@gmail.com>
2020-04-28 22:25:37 -0300

    Inicio da aula 3 e com novo projeto

---
+++ Aulas/Aula2/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions option) : base(option)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Adiciona a classe no modelo para gerar o banco
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+        }
+    }
+}
